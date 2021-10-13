8d431354a8866f7318b7a60fe73e2f78f1ac5f0c
maasa-k <maasa.dev@gmail.com>
2021-10-12 20:18:07 -0700

    Able to fetch data directly from RapidAPI so Rails API for this app is unnecessary

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-// import WordForm from "./WordForm";
+import WordForm from "./WordForm";
 // import Result from "./Result";
 
 const App = () => {
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(`http://localhost:3001/api/v1/words`);
-            const newData = await response.json();
-            setDefinition(newData);
-        };
-        fetchData();
-    }, []);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const response = await fetch(`http://localhost:3001/api/v1/words`);
+    //         const newData = await response.json();
+    //         setDefinition(newData);
+    //     };
+    //     fetchData();
+    // }, []);
+
+    fetch("https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=horse", {
+        method: "GET",
+        headers: {
+            "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com",
+            "x-rapidapi-key": "caaac4088amsh273e15f4876e7f1p170c48jsnc31e01bd75c1",
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => {
+            console.error(err);
+        });
 
     // const [word, setWord] = useState("");
     const [definition, setDefinition] = useState("");
-    console.log(definition);
 
     // const getWord = (word) => {
     //     setWord(word);
@@ -30,8 +42,9 @@ const App = () => {
             <h1>Learn the Word</h1>
             <br />
 
-            {/* <WordForm getWord={getWord} getDefinition={getDefinition} />
-            <br />
+            <WordForm />
+            {/* <WordForm getWord={getWord} getDefinition={getDefinition} /> */}
+            {/* <br />
             <br />
             <Result word={word} definition={definition} /> */}
         </div>
