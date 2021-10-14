0a470f6f032c92867f70fb9f680d903313668c37
maasa-k <maasa.dev@gmail.com>
2021-10-13 20:41:17 -0700

    Render definition only if resultCode is 200

--- src/Result.js
+++ src/Result.js
@@ -3,24 +3,36 @@ import React from 'react';
 const Result = (props) => {
     const word = props.word && props.word;
 
-    const noun = props.definition && props.definition.meaning.noun;
-    const verb = props.definition && props.definition.meaning.verb;
-    const adverb = props.definition && props.definition.meaning.adverb;
-    const adjective = props.definition && props.definition.meaning.adjective;
+    const resultCode = props.definition.result_code;
+    // If resultCode === "200" render definition, else render error message
 
-    return (
-        <div>
-            <h1>{word}</h1>
+    if (resultCode === '200') {
+        const noun = props.definition && props.definition.meaning.noun;
+        const verb = props.definition && props.definition.meaning.verb;
+        const adverb = props.definition && props.definition.meaning.adverb;
+        const adjective = props.definition && props.definition.meaning.adjective;
 
-            <h3>Definitions:</h3>
-            <p>{noun}</p>
-            <p>{verb}</p>
-            <p>{adverb}</p>
-            <p>{adjective}</p>
+        return (
+            <div>
+                <h1>{word}</h1>
 
-            <h3>Synonyms:</h3>
-        </div>
-    );
+                <h3>Definitions:</h3>
+                <li>{noun}</li>
+                <li>{verb}</li>
+                <li>{adverb}</li>
+                <li>{adjective}</li>
+
+                <h3>Synonyms:</h3>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h1>{word}</h1>
+                <h2>NO DEFINITION FOUND</h2>
+            </div>
+        );
+    }
 };
 
 export default Result;
