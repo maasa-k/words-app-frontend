bca8741fefcafb30fc2f1845fbb9e3e09df670b6
maasa-k <maasa.dev@gmail.com>
2021-10-17 14:36:36 -0700

    Stylize the search input field

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,53 @@
 import React, { useState } from 'react';
+import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Input from '@mui/material/Input';
-import Button from '@mui/material/Button';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+   position: 'relative',
+   borderRadius: theme.shape.borderRadius,
+   backgroundColor: alpha(theme.palette.common.white, 0.15),
+   '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+   },
+   marginLeft: 0,
+   width: '100%',
+   [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+   },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+   padding: theme.spacing(0, 2),
+   height: '100%',
+   position: 'absolute',
+   pointerEvents: 'none',
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+   color: 'inherit',
+   '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+         width: '12ch',
+         '&:focus': {
+            width: '20ch',
+         },
+      },
+   },
+}));
 
 const Navbar = (props) => {
    const [word, setWord] = useState('');
@@ -39,10 +82,18 @@ const Navbar = (props) => {
                   Learn the Word
                </Typography>
                <form onSubmit={handleSubmit}>
-                  <Input placeholder='Type in word here' value={word} name={word} onChange={handleChange} />
-                  <Button color='secondary' type='submit'>
-                     Find Definition
-                  </Button>
+                  <Search>
+                     <SearchIconWrapper>
+                        <SearchIcon />
+                     </SearchIconWrapper>
+                     <StyledInputBase
+                        placeholder='Type here...'
+                        inputProps={{ 'aria-label': 'search' }}
+                        value={word}
+                        name={word}
+                        onChange={handleChange}
+                     />
+                  </Search>
                </form>
             </Toolbar>
          </AppBar>
@@ -51,6 +102,26 @@ const Navbar = (props) => {
 };
 
 export default Navbar;
+
+{
+   /* <Box sx={{ flexGrow: 1 }}>
+<AppBar position='static'>
+   <Toolbar>
+      <Typography variant='h6' noWrap component='div' sx={{ textAlign: 'left', flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+         Learn the Word
+      </Typography>
+      <form onSubmit={handleSubmit}>
+         <Input placeholder='Type in word here' value={word} name={word} onChange={handleChange} />
+         <Button color='secondary' type='submit'>
+            Find Definition
+         </Button>
+      </form>
+   </Toolbar>
+</AppBar>
+</Box>  */
+}
+
+// ------------------------------------------------------------------------------- //
 
 // import React from 'react';
 // import AppBar from '@mui/material/AppBar';
